3b423ae488ef49f02973b090e7aa385188ff9a8e
gbln-army <dopo15yes@gmail.com>
2025-07-06 19:51:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("start", start))
 
     app.run_polling()
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Dino Miner Bot Aktif!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+threading.Thread(target=run).start()
