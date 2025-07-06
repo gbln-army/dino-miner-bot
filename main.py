9487096bc57801ea4f7a8566508a64557bbc91f9
gbln-army <dopo15yes@gmail.com>
2025-07-06 20:21:13 +0700

    main.py

--- main.py
+++ main.py
@@ -26,3 +26,5 @@ def run():
     app.run(host='0.0.0.0', port=8080)
 
 threading.Thread(target=run).start()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
